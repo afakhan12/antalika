b6ff00f36037a223b8f71b71b5f69e913830569e
afakhan12 <fakhani.automation@gmail.com>
2025-12-28 17:25:22 +0300

    fix config file

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
+const repo = "antalika";
+
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  output: "export",
+  images: { unoptimized: true },
   trailingSlash: true,
-}
 
-module.exports = nextConfig
+  // ✅ IMPORTANT for GitHub Pages project site
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+};
 
+module.exports = nextConfig;
